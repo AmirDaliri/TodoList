87daccb97598017d4704ef800587347c93298d91
amir daliri <amir.daliri@mobilion.com.tr>
2019-09-13 12:57:14 +0300

    code convention

---
+++ TestTodoList(realm)/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTodoList(realm)
+//
+//  Created by Amir Daliri on 9/13/19.
+//  Copyright © 2019 Amir Daliri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
