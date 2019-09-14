29efeab98862495d852384389db0ba02f2f28429
idaliri <idaliri69@gmail.com>
2019-09-14 20:18:15 +0300

    feature add core data model and add, delete, update

--- TestTodoList_CoreData/Controllers/TodoViewController.swift
+++ TestTodoList_CoreData/Controllers/TodoViewController.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var lists : Results<TodoList>!
+    var list : [Todo] = []
     var currentCreateAction:UIAlertAction!
     var isEditingMode = false
 
+    // Managed Object Context
+    let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
     // MARK: - Lifecycle Methods
     
     override func viewDidLoad() {
@@ -30,92 +35,70 @@ class TodoViewController: UIViewController {
         fetchData()
     }
     
-    
-    // MARK: - Fetch Data Methode
-
-    func fetchData() {
-        lists = uiRealm.objects(TodoList.self)
-        self.tableView.setEditing(false, animated: true)
-        self.tableView.reloadData()
-    }
-    
     // MARK: - Alert Methode
     
-    func showListAlert(_ updatedList: TodoList!) {
-        var title = "New Tasks List"
-        var doneTitle = "Create"
-        if updatedList != nil{
-            title = "Update Tasks List"
-            doneTitle = "Update"
-        }
-
-        let alert = UIAlertController(title: title, message: "Write the name of your tasks list.", preferredStyle: .alert)
-        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: {(Bool) in })
-        let action = UIAlertAction(title: doneTitle, style: .default) { (actopn) in
-            let listName = alert.textFields?.first?.text
-            self.listAlertAction(updatedList, listName: listName)
+    private func showAddAlert() {
+        let alert = UIAlertController(
+            title: "New Task",
+            message: "What do you want to do?",
+            preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text,
+                !task.isEmpty else { return }
+            
+            self.save(task)
+            
         }
         
-        alert.addAction(action)
-        action.isEnabled = false
-        self.currentCreateAction = action
-        alert.addAction(cancel)
-
-        alert.addTextField { (textField) in
-            textField.placeholder = "Task List Name"
-            textField.addTarget(self, action: #selector(self.listNameFieldDidChange(_:)), for: UIControl.Event.editingChanged)
-            if updatedList != nil{
-                textField.text = updatedList.subject
-            }
-        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
         
-        self.present(alert, animated: true, completion:nil)
+        alert.addTextField()
+        alert.textFields?.first?.keyboardAppearance = .dark
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
     }
 
+    func showEditAlert(at row: Int) {
+        let alert = UIAlertController(
+            title: "Edit task",
+            message: "Do you want to change this?",
+            preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text,
+                !task.isEmpty else { return }
+            
+            self.update(at: row, newTaskName: task)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alert.addTextField()
+        alert.textFields?.first?.keyboardAppearance = .dark
+        alert.textFields?.first?.text = list[row].name
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
     
     // MARK: - Action Methode
-    
-    func listAlertAction(_ updatedList: TodoList!, listName: String?) {
-        if updatedList != nil{
-            // update mode
-            try! uiRealm.write{
-                updatedList.subject = listName!
-                self.fetchData()
-            }
-        }
-        else{
-            let newTaskList = TodoList()
-            newTaskList.subject = listName!
-            
-            try! uiRealm.write{
-                
-                uiRealm.add(newTaskList)
-                self.fetchData()
-            }
-        }
-    }
+
     
     @objc func listNameFieldDidChange(_ textField:UITextField){
         self.currentCreateAction.isEnabled = (textField.text?.count)! > 0
     }
 
     @IBAction func addBarButtonItemTapped(_ sender: UIBarButtonItem) {
-        showListAlert(nil)
+        showAddAlert()
     }
     
     @IBAction func editBarButtonItemTapped(_ sender: UIBarButtonItem) {
         isEditingMode = !isEditingMode
         self.tableView.setEditing(isEditingMode, animated: true)
-    }
-    
-    @IBAction func refreshBarButtonItemTapped(_ sender: UIBarButtonItem) {
-        Helpers.showSortActionSheetAlert(self, atoZHandler: { (actino) in
-            self.lists = self.lists.sorted(byKeyPath: "subject")
-            self.tableView.reloadData()
-        }) { (actino) in
-            self.lists = self.lists.sorted(byKeyPath: "createdAt", ascending:false)
-            self.tableView.reloadData()
-        }
     }
 }
 
@@ -124,43 +107,115 @@ class TodoViewController: UIViewController {
 extension TodoViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let list = lists {
-            return list.count
-        }
-        return 0
+        return list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoTableViewCell", for: indexPath)
-        let list = lists[indexPath.row]
-        cell.textLabel?.text = list.subject
-        cell.detailTextLabel?.text = "\(list.todos.count) Todo"
+        cell.textLabel?.text = list[indexPath.row].name
         return cell
     }
     
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (deleteAction, indexPath) -> Void in
-            
-            // TODO:  Deletion
-            let listToBeDeleted = self.lists[indexPath.row]
-            try! uiRealm.write{
-                
-                uiRealm.delete(listToBeDeleted)
-                self.fetchData()
-            }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            delete(at: indexPath.row)
         }
-        
-        let editAction = UITableViewRowAction(style: UITableViewRowAction.Style.normal, title: "Edit") { (editAction, indexPath) -> Void in
-            
-            // TODO:  Editing
-            let listToBeUpdated = self.lists[indexPath.row]
-            self.showListAlert(listToBeUpdated)
-            
-        }
-        return [deleteAction, editAction]
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showEditAlert(at: indexPath.row)
+
+    }
+}
+
+
+
+
+extension TodoViewController {
+    
+    // MARK: - Work with Core Data
+    func save(_ taskName: String) {
         
+        // Entity Description
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Todo", in: managedContext) else { return }
+        
+        // Entity (Task model instance from Core Data)
+        let task = NSManagedObject(entity: entityDescription, insertInto: managedContext) as! Todo
+        task.name = taskName
+        
+        // Saving
+        do {
+            try managedContext.save()
+            list.append(task)
+            self.tableView.insertRows(
+                at: [IndexPath(row: self.list.count - 1, section: 0)],
+                with: .automatic)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    // MARK: - Fetch Data
+
+    func fetchData() {
+        
+        let fetchRequest: NSFetchRequest<Todo> = Todo.fetchRequest()
+        
+        do {
+            list = try managedContext.fetch(fetchRequest)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    // MARK: - Delete Data
+
+    func delete(at index: Int) {
+        let fetchRequest: NSFetchRequest<Todo> = Todo.fetchRequest()
+        
+        do {
+            let tasks = try managedContext.fetch(fetchRequest)
+            let taskToDelete = tasks[index] as NSManagedObject
+            managedContext.delete(taskToDelete)
+            self.list.remove(at: index)
+            tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+            
+            do {
+                try managedContext.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    // MARK: - Update Data
+
+    func update(at index: Int, newTaskName: String) {
+        
+        let fetchRequest: NSFetchRequest<Todo> = Todo.fetchRequest()
+        
+        do {
+            let tasks = try managedContext.fetch(fetchRequest)
+            let taskToUpdate = tasks[index] as NSManagedObject
+            taskToUpdate.setValue(newTaskName, forKey: "name")
+            
+            self.list[index].name = newTaskName
+            self.tableView.reloadRows(
+                at: [IndexPath(row: index, section: 0)],
+                with: .automatic)
+            
+            do {
+                try managedContext.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
